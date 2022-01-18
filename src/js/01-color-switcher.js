abea2af4226bd2e043da46dc5ec60e4694251c12
Mykhailo Sokyrko <sokirko.mm@gmail.com>
2022-01-19 00:23:05 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+
+const bntStart = document.querySelector('[data-start]');
+const bntStop = document.querySelector('[data-stop]');
+let timerId = null;
+bntStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    bntStart.setAttribute("disabled", "disabled");
+});
+
+bntStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    bntStart.removeAttribute("disabled");
+});
